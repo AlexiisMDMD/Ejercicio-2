e489247cca953fb4abca72443d11cf8f6d99b28a
AlexiisMDMD <136538197+AlexiisMDMD@users.noreply.github.com>
2023-06-14 20:57:32 -0400

    Initial commit

---
+++ Ejercicio #2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+  
+class Program{
+
+     public static void Main(string[] args){
+
+
+        int numeroGanador = 4;
+        bool numeroAdivinado = false;
+
+
+        
+        while (!numeroAdivinado)
+        {// el bucle while hace que el usuario vuelva a introducir un numero si el que introdujo no es el correcto// 
+
+            Console.WriteLine("Introduce un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            if (numero == numeroGanador)
+            {
+
+                Console.WriteLine("Haz adivinado el numero ganador");
+                numeroAdivinado = true;
+
+            }
+
+            else {
+                Console.WriteLine("No adivinaste el numero (Introduce otro numero)");
+         
+            }
+
+        }
+   
+     }
+
+}
